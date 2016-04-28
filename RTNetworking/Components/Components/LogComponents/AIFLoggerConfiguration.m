dc379a9e2cb9f301dffd5dde573e6aa93b5bb718
ChenZhiWen <czwen1993@gmail.com>
2016-04-28 12:50:50 +0800

    去除写死的东西

--- RTNetworking/Components/Components/LogComponents/AIFLoggerConfiguration.m
+++ RTNetworking/Components/Components/LogComponents/AIFLoggerConfiguration.m
@@ -13,40 +13,14 @@
 
 - (void)configWithAppType:(AIFAppType)appType
 {
-    switch (appType) {
-        case AIFAppTypeAnjuke:
-            self.channelID = [AIFAppContext sharedInstance].channelID;
-            self.appKey = @"appKey";
-            self.logAppName = [AIFAppContext sharedInstance].appName;
-            self.serviceType = kAIFServiceAnjuke;
-            self.sendLogMethod = @"admin.writeAppLog";
-            self.sendActionMethod = @"admin.recordaction";
-            self.sendLogKey = @"data";
-            self.sendActionKey = @"action_note";
-            break;
-            
-        case AIFAppTypeBroker:
-            self.channelID = [AIFAppContext sharedInstance].channelID;
-            self.appKey = @"appKey";
-            self.logAppName = [AIFAppContext sharedInstance].appName;
-            self.serviceType = kAIFServiceBrokerRest;
-            self.sendLogMethod = @"admin.writeAppLog";
-            self.sendActionMethod = @"nlog/";
-            self.sendLogKey = @"data";
-            self.sendActionKey = @"log";
-            break;
-            
-        default:
-            self.channelID = [AIFAppContext sharedInstance].channelID;
-            self.appKey = @"appKey";
-            self.logAppName = [AIFAppContext sharedInstance].appName;
-            self.serviceType = kAIFServiceAnjuke;
-            self.sendLogMethod = @"admin.writeAppLog";
-            self.sendActionMethod = @"admin.recordaction";
-            self.sendLogKey = @"data";
-            self.sendActionKey = @"action_note";
-            break;
-    }
+    self.channelID = [AIFAppContext sharedInstance].channelID;
+    self.appKey = @"appKey";
+    self.logAppName = [AIFAppContext sharedInstance].appName;
+//            self.serviceType = kAIFServiceAnjuke;
+    self.sendLogMethod = @"admin.writeAppLog";
+    self.sendActionMethod = @"admin.recordaction";
+    self.sendLogKey = @"data";
+    self.sendActionKey = @"action_note";
 }
 
 @end
