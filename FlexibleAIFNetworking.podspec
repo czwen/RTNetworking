83755b29938fed610d5a2f866595d24b2079e58c
ChenZhiWen <czwen1993@gmail.com>
2016-04-28 17:04:59 +0800

    add FlexibleAIFNetworking.h

--- FlexibleAIFNetworking.podspec
+++ FlexibleAIFNetworking.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
   s.name     = "FlexibleAIFNetworking"
 
-  s.version  = "1.0"
+  s.version  = "1.0.0"
 
   s.license = { :type => "MIT" }
 
---
+++ RTNetworking/Components/FlexibleAIFNetworking.h
@@ -0,0 +1,9 @@
+//
+//  FlexibleAIFNetworking.h
+//  RTNetworking
+//
+//  Created by ChenZhiWen on 16/4/28.
+//  Copyright © 2016 anjuke. All rights reserved.
+//
+
+#import "AIFNetworking.h"
