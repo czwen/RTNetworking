0d5bb30bcb70d5a9a2627d21b26c5cb80b939fa3
ChenZhiWen <czwen1993@gmail.com>
2016-08-16 10:43:32 +0800

    optional method

--- RTNetworking/Components/Components/RTAPIBaseManager.h
+++ RTNetworking/Components/Components/RTAPIBaseManager.h
@@ -49,10 +49,10 @@ static NSString * const kRTAPIBaseManagerRequestID = @"kRTAPIBaseManagerRequestI
 //api回调
 @protocol RTAPIManagerApiCallBackDelegate <NSObject>
  @required
-
-- (void)managerCallAPIProgress:(RTAPIBaseManager *)manager percentage:(CGFloat)percentage;
 - (void)managerCallAPIDidSuccess:(RTAPIBaseManager *)manager;
 - (void)managerCallAPIDidFailed:(RTAPIBaseManager *)manager;
+@optional
+- (void)managerCallAPIProgress:(RTAPIBaseManager *)manager percentage:(CGFloat)percentage;
 @end
 
 
